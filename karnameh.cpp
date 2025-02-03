79cdb44b52b8fda82276abedbf7c597636584f9a
HYFR1385 <steavegalahant@gmail.com>
2025-02-03 23:43:49 +0330

    Update karnameh.cpp

--- karnameh.cpp
+++ karnameh.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include <iomanip>
 #include<cstring>
 #include<algorithm>
 using namespace std;
@@ -22,11 +23,11 @@ bool list(student &a, student &b){
 }
   void addlesson(lesson & l,student &s){
 
-  cout<<"Enter name of lesson :";
+  cout<<"\nEnter name of lesson :\n";
   cin>>l.Lname;
-  cout<<"Enter count of credits :";
+  cout<<"Enter count of credits :\n";
   cin>>l.credit;
-  cout<<" Enter point of lesson";
+  cout<<" Enter point of lesson\n";
   cin >> l.point;
     s.GPA += l.point;
 
@@ -34,11 +35,14 @@ bool list(student &a, student &b){
   }
   
  void addstude(student &s){
-  cout<<"\nEnter name :";
+  punch :
+  cout<<"\nEnter name :\n";
    cin.ignore();
    getline(cin,s.name);
+   if(!(getline(cin,s.name))){
    cout <<"\nEnter ID :\n";
    cin>>s.ID;
+   if(cin>>s.ID){
    cout <<"\nEnter field :\n";
    cin>> s.field;
    s.GPA =0;
@@ -51,20 +55,65 @@ bool list(student &a, student &b){
    addlesson(l,s);
    s.lessons.push_back(l);
    }
+   
 
    s.GPA /= numlesson;
     cout<<"---------------------------------------\n";
-    cout<<" had done compeletly !";
+    cout<<" had done compeletly !\n\n";
+   }
+     }    
+     else{
+     cout<<"wrong answer.return again"  ;
+      goto punch;}
+     
+ }
 
-     }      
 
-    show(vector <student>students){
+   void show(vector <student>students){
+      cout << "\n________________________________________\n";
+    cout << left << setw(15) << "| Name |" << setw(15) << "| ID |" << setw(15) << "| Field  |" << setw(10) << "| GPA |" << endl;
+    cout << "----------------------------------------\n";
+    for (int i = 0; i < students.size(); i++) {
+        cout << left << setw(15) <<" | "<< students[i].name<< setw(15) <<" | "<< students[i].ID<< setw(15) <<" | "<< students[i].field<< setw(10) << fixed << setprecision(2) <<" | "<< students[i].GPA<<" |";
+         cout << "\n----------------------------------------\n\n";
+       }
        
+    }
+    void speceficField(string &spece,vector<student> &students){
+      bool test=false;
+        cout << left << setw(15) << "| Name |" << setw(15) << "| ID |" << setw(15) << "| Field  |" << setw(10) << "| GPA |" << endl;
+      for(int i=0; i < students.size(); i++) {
+        cout << "\n----------------------------------------\n";
+        if(spece==students[i].field){
+        cout << left << setw(15) <<" | "<< students[i].name<< setw(15) <<" | "<< students[i].ID<< setw(15) <<" | "<< students[i].field<< setw(10) << fixed << setprecision(2) <<" | "<< students[i].GPA<<" |";
+        test=true;
 
-    } 
+      }   
+     else
+       cout<<"\nNot FOUND !";
 
+
+    }    
+    }
+    
+    void reportCard(string &name, int& sID, vector<student>students)
+    {
+      cout << "\n----------------------------------------\n";
+     for (int i =0 ; i< students.size() ; i++){
+      if (name == students[i].name && sID == students[i].ID){
+        cout << "Name: " << students[i].name << setw(10) << right << "ID: " << students[i].ID << endl;
+            cout << setw(15) << left << "| Lesson |" << setw(15) << "| Credit |" << setw(15) << "| Point |" << endl;
+            for (int j =0 ; j < students[i].lessons.size(); j++) {
+                 cout << setw(15) << left << students[i].lessons[j].Lname  << setw(15) << students[i].lessons[j].credit<< setw(15) << students[i].lessons[j].point <<"\n";
+            }
+     }
+     else
+       cout<<"\nNOT FOUND !!";
+     }
+     }
 int main(){
-int n,num;
+int n,num,sID;
+string spece,name;
 vector <student> students;
 do 
 {
@@ -77,19 +126,44 @@ do
 switch (n)
 {
 case 1:{
-    cout<<"Enter number of student :\n";
+   jump: 
+   cout<<"Enter number of student :\n";
     cin>>num;
+    if(cin>>num){
     for (int i = 0 ; i<num; i++){
     student s;
     addstude(s);
     students.push_back(s);
     }
     }
+    else
+    {
+      cout<<"wrong answer.return again";
+      goto jump;
+    }
+    
+    }
     break;
     case 2 : {
      sort(students.begin(),students.end(),list);
       show(students);
-
+     break;
+    }
+    case 3:
+    {
+    cout<<"\nEnter your specefic field :\n ";
+    cin>>spece;
+    speceficField(spece,students);
+    break;
+    }
+    case 4:
+    {
+      cout<<"Enter the student name:\n";
+      cin >>name; 
+      cout<<"\nEnter the student ID:\n";
+      cin>>sID;
+      reportCard(name,sID,students);
+      break;
     }
 default:
     break;
